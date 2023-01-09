7131aef686e4d1e22bf7ed9df8b8bfa87d7c4298
developers@techstation.com.eg <developers@techstation.com.eg>
2023-01-09 18:01:28 +0530

    Tool Updated

--- order_update/order_update/doctype/search_and_update_orders/search_and_update_orders.js
+++ order_update/order_update/doctype/search_and_update_orders/search_and_update_orders.js
@@ -3,52 +3,122 @@
 
 frappe.ui.form.on('Search and Update Orders', {
 	onload: function(frm) {
+		frm.set_value("company", "");
 		frm.set_value("customer", "");
 		frm.set_value("order_type", "");
 		frm.set_value("status", ""); 
 		frm.set_value("from_date", "");
 		frm.set_value("to_date", "");
 		frm.set_value("import_file", "");
+		frm.clear_table('searched_order_list');
+		frm.refresh_field('searched_order_list');
 	},
 	refresh: function(frm) {
+		if (frm.$wrapper.find(".layout-side-section").css("display") == "block") {
+			frm.$wrapper.find(".layout-side-section").css("display", "none");
+		}
+		frm.fields_dict['searched_order_list'].grid.wrapper.find('.grid-add-row').hide();
+		frm.get_field('searched_order_list').grid.df.cannot_delete_rows = true;
+		frm.get_field('searched_order_list').grid.df.cannot_add_rows = true;
+
 		frm.add_custom_button(__("Order List"), function() {
 			frappe.set_route("List", "Sales Order");
 		});
 		frm.disable_save();
 		frm.page.set_primary_action(__('Process'), () => {
-			if(frm.doc.import_file){
-				process_import(frm);
-			}
-			else{
-				frappe.throw(__('Please attach file to import'));
+			if(frm.doc.searched_order_list && frm.doc.searched_order_list.length > 0){
+				process_update(frm);
 			}
 		});
+	},
+	company: function(frm) {
+		set_orders_list(frm);
+	},
+	customer: function(frm) {
+		set_orders_list(frm);
+	},
+	order_type: function(frm) {
+		set_orders_list(frm);
+	},
+	status: function(frm) {
+		set_orders_list(frm);
+	},
+	from_date: function(frm) {
+		set_orders_list(frm);
+	},
+	to_date: function(frm) {
+		set_orders_list(frm);
 	},
 	download_template(frm) {
 		open_url_post(
 			'/api/method/order_update.order_update.doctype.search_and_update_orders.search_and_update_orders.download_template',
-			{
-				company : frm.doc.company,
-				customer : frm.doc.customer,
-				order_type : frm.doc.order_type,
-				status :frm.doc.status,
-				from_date : frm.doc.from_date,
-				to_date : frm.doc.to_date
-			}
+			{}
 		);
+	},
+	after_save: function(frm) {
+		if(frm.doc.import_file){
+			set_excel_records(frm);
+		}
 	}
 });
 
-function process_import(frm) {
-	frm.page.page_actions.find(".primary-action").addClass("hide");
-	frm.page.set_indicator(__('In Progress'), 'green');
+function set_orders_list(frm) {
+	if(!frm.doc.import_file){
+		frm.clear_table('searched_order_list');
+		frm.refresh_field('searched_order_list');
 		frappe.call({
-			method: "process_data_excel",
+			method: "get_order_data",
 			doc:frm.doc,
 			freeze: true,
-			freeze_message: __("Updating Data..."),
-		callback: function(r) {
-			frm.page.set_indicator(__('Records Updated'), 'green');
+			freeze_message: __("Fetching Data..."),
+			callback: function(r) {
+				$.each(r.message, function(index, row){
+					let d = frm.add_child('searched_order_list');
+						d.order_no = row.name;
+						d.date = row.transaction_date;
+						d.status = row.status;
+						d.shipping_status = row.shipping_status;
+						d.tracking_number = row.tracking_number;
+					frm.refresh_field('searched_order_list');
+				});
+			}
+		});
+	}
+}
+
+function set_excel_records(frm) {
+	frm.clear_table('searched_order_list');
+	frm.refresh_field('searched_order_list');
+	frappe.call({
+		method: "get_excel_record",
+		doc:frm.doc,
+		freeze: true,
+		freeze_message: __("Updating Data..."),
+	callback: function(r) {
+		$.each(r.message, function(index, row){
+			let d = frm.add_child('searched_order_list');
+				d.order_no = row.order_no;
+				d.date = row.date;
+				d.status = row.status;
+				d.shipping_status = row.shipping_status;
+				d.tracking_number = row.tracking_number;
+			frm.refresh_field('searched_order_list');
+		});
+	}
+});
+}
+
+function process_update(frm) {
+	frappe.call({
+		method: "process_update",
+		doc:frm.doc,
+		freeze: true,
+		freeze_message: __("Updating Data..."),
+	callback: function(r) {
+		if(r.message == true){
+			frappe.show_alert('Records Updated')
+			frm.reload_doc();
 		}
-	});
+	}
+});
 }
--- order_update/order_update/doctype/search_and_update_orders/search_and_update_orders.py
+++ order_update/order_update/doctype/search_and_update_orders/search_and_update_orders.py
@@ -14,68 +14,130 @@ import base64
 from six import string_types
 import json
 
+STATUS_COLOR = {
+		"Draft": "red",
+		"On Hold": "blue",
+		"To Bill": "orange",
+		"To Deliver and Bill": "green",
+		"To Deliver": "orange",
+		"Cancelled": "red",
+		"Completed": "green",
+		"Closed": "blue"
+		}
+
 class SearchandUpdateOrders(Document):
 	@frappe.whitelist()
-	def process_data_excel(self):
+	def get_excel_record(self):
 		data = read_xlsx_file_from_attached_file(self.import_file)
 		if not len(data[1:]):
 			frappe.throw(_("Excel Sheet must contains records to update"))
 
 		else:
-			frappe.enqueue(update_searched_orders,timeout=6000,is_async=True,now=True,doc=self, data=data)
+			order_data = []
+			for f in data[1:]:
+				if f[0] and frappe.db.exists("Sales Order", str(f[0])):
+					so_status = frappe.db.get_value('Sales Order', f[0], 'status')
+					return_data = {}
+					return_data['order_no'] = f[0]
+					return_data['date'] = frappe.db.get_value('Sales Order', f[0], 'transaction_date')
+					return_data['status'] = "<span class=\"indicator-pill whitespace-nowrap {color}\"> <span>{status}</span></span>".format(
+					color = STATUS_COLOR[so_status],status = so_status) 
+					return_data['shipping_status'] = frappe.db.get_value('Sales Order', f[0], 'shipping_status')
+					return_data['tracking_number'] = frappe.db.get_value('Sales Order', f[0], 'tracking_number')
+					order_data.append(return_data)
+		
+			return order_data
 
-def update_searched_orders(doc, data, publish_progress =True):
-	fields = ["Sales Order","Shipping Status","Tracking Number"]
+	@frappe.whitelist()
+	def get_order_data(self):
+		filters = []
+		if self.company :
+			filters.append(['company', '=', self.company])
+		if self.customer :
+			filters.append(['customer', '=', self.customer])
+		if self.order_type :
+			filters.append(['order_type', '=', self.order_type])
+		if self.status :
+			filters.append(['status', '=', self.status])
+		if self.from_date :
+			filters.append(['transaction_date', '>=', self.from_date])
+		if self.to_date :
+			filters.append(['transaction_date', '<=', self.to_date])
 
-	if data[0] != fields:
-		frappe.throw(_("Column in Excel Sheet are not matched with default column"))
+		sales_order_records = frappe.db.get_list('Sales Order',filters=filters , 
+		fields = ['name','transaction_date','status','shipping_status','tracking_number'])
+		return sales_order_records if filters else []
 
-	rownum = 0
-	for f in data[1:]:
-		rownum +=1
-		if frappe.db.exists("Sales Order", str(f[0])) and (f[1] or f[2]):
-			frappe.db.set_value('Sales Order', f[0], 'shipping_status', f[1])
-			frappe.db.set_value('Sales Order', f[0], 'tracking_number', f[2])
+	@frappe.whitelist()
+	def process_update(self):
+		data = read_xlsx_file_from_attached_file(self.import_file)
+		fields = ["Sales Order"]
+		if data[0] != fields:
+			frappe.throw(_("Column in Excel Sheet are not matched with default column"))
+
+		for f in self.searched_order_list:
+			if frappe.db.exists("Sales Order", f.order_no):
+				frappe.db.set_value('Sales Order', f.order_no, 'shipping_status', f.shipping_status)
+				frappe.db.set_value('Sales Order', f.order_no, 'tracking_number', f.tracking_number)
+
+		return True
+
+# Privously Used Code
+
+# def update_searched_orders(doc, data, publish_progress =True):
+# 	order_data = []
+
+# 	for f in data[1:]:
+# 		if f[0] and frappe.db.exists("Sales Order", str(f[0])):
+# 			return_data = {}
+# 			return_data['order_no'] = f[0]
+# 			return_data['date'] = frappe.db.get_value('Sales Order', f[0], 'transaction_date')
+# 			return_data['status'] = frappe.db.get_value('Sales Order', f[0], 'status')
+# 			return_data['shipping_status'] = frappe.db.get_value('Sales Order', f[0], 'shipping_status')
+# 			return_data['tracking_number'] = frappe.db.get_value('Sales Order', f[0], 'tracking_number')
+# 			order_data.append(return_data)
+		
+# 	return order_data
 
 @frappe.whitelist()
-def download_template(company = None, customer = None,order_type = None,status = None,from_date = None,to_date = None):
+def download_template():
 	data = frappe._dict(frappe.local.form_dict)
-	writer = get_template(company = None, customer = customer,order_type = order_type,status = status,from_date = from_date,to_date=to_date)
+	writer = get_template()
 	build_response_as_excel(writer)
 
-def get_template(company,customer,order_type,status,from_date,to_date):
+def get_template():
 	import json
 	fields = []
 
-	fields = ["Sales Order","Shipping Status","Tracking Number"]
+	fields = ["Sales Order"]
 	writer = UnicodeWriter()
 	writer.writerow(fields)
-	writer = get_sample_template(writer=writer,company = None,customer = customer,order_type = order_type,status = status,from_date = from_date,to_date=to_date)
+	# writer = get_sample_template(writer=writer,company = None,customer = customer,order_type = order_type,status = status,from_date = from_date,to_date=to_date)
 
 	return writer
 
-def get_sample_template(writer,company,customer,order_type,status,from_date,to_date):
-	row = []
+# def get_sample_template(writer,company,customer,order_type,status,from_date,to_date):
+# 	row = []
 
-	filters = []
-	if company :
-		filters.append(['company', '=', company])
-	if customer :
-		filters.append(['customer', '=', customer])
-	if order_type :
-		filters.append(['order_type', '=', order_type])
-	if status :
-		filters.append(['status', '=', status])
-	if from_date :
-		filters.append(['transaction_date', '>=', from_date])
-	if to_date :
-		filters.append(['transaction_date', '<=', to_date])
+# 	filters = []
+# 	if company :
+# 		filters.append(['company', '=', company])
+# 	if customer :
+# 		filters.append(['customer', '=', customer])
+# 	if order_type :
+# 		filters.append(['order_type', '=', order_type])
+# 	if status :
+# 		filters.append(['status', '=', status])
+# 	if from_date :
+# 		filters.append(['transaction_date', '>=', from_date])
+# 	if to_date :
+# 		filters.append(['transaction_date', '<=', to_date])
 
-	sales_order_records = frappe.db.get_list('Sales Order',filters=filters , fields = ['name','shipping_status','tracking_number'])
-	for so in sales_order_records:
-		row = [so.name,so.shipping_status,so.tracking_number]
-		writer.writerow(row)
-	return writer	
+# 	sales_order_records = frappe.db.get_list('Sales Order',filters=filters , fields = ['name','shipping_status','tracking_number'])
+# 	for so in sales_order_records:
+# 		row = [so.name,so.shipping_status,so.tracking_number]
+# 		writer.writerow(row)
+# 	return writer	
 
 def build_response_as_excel(writer):
 	filename = frappe.generate_hash("", 10)
